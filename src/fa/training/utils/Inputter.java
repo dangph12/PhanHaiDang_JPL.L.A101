1060d7f860c9824a783782ef7476b41c7fbc6268
dangph12 <dangphhe181217@fpt.edu.vn>
2025-01-10 15:53:54 +0700

    feat: Input string

--- src/fa/training/utils/Inputter.java
+++ src/fa/training/utils/Inputter.java
@@ -19,4 +19,23 @@ public class Inputter {
         }
         return result;
     }
+
+    public String inputString(String message) {
+        boolean isContinue = true;
+        String result = "";
+        while (isContinue) {
+            try {
+                System.out.print(message);
+                result = scanner.nextLine();
+                if (result.equalsIgnoreCase("")) {
+                    throw new Exception();
+                }
+                isContinue = false;
+            } catch (Exception e) {
+                System.out.println("Please enter a non-empty string.");
+            }
+        }
+        return result;
+    }
+
 }
