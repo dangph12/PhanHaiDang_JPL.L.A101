ab06545f42e51edb40be777229452643c434eb7b
dangph12 <dangphhe181217@fpt.edu.vn>
2025-01-10 15:54:19 +0700

    Create PublicationService.java

---
+++ src/fa/training/services/PublicationService.java
@@ -0,0 +1,36 @@
+package fa.training.services;
+
+import fa.training.utils.Inputter;
+import fa.training.utils.Validator;
+
+import java.util.Date;
+
+public class PublicationService {
+
+    private final Inputter inputter = new Inputter();
+    private final Validator validator = new Validator();
+
+    public int inputPublicationYear() {
+        int publicationYear = 0;
+        boolean isContinue = true;
+        while (isContinue) {
+            publicationYear = inputter.inputInteger("Enter publication year (year >= 1900): ");
+            if (!validator.isValidYear(publicationYear)) {
+                System.out.println("Enter year after 1900");
+            } else {
+                isContinue = false;
+            }
+        }
+        return publicationYear;
+    }
+
+    public String inputPublisher() {
+        String publisher = inputter.inputString("Enter publisher: ");
+        return publisher;
+    }
+
+    public Date inputPublicationDate() {
+        Date publicationDate = inputter.inputDate("Enter publication date (dd-MM-yyyy): ");
+        return publicationDate;
+    }
+}
