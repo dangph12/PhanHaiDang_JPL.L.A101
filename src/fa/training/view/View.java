5fc14216f2ef897503a66386bd73b3e6a4c4f8a9
dangph12 <dangphhe181217@fpt.edu.vn>
2025-01-13 15:18:09 +0700

    chore: Fix typo

--- src/fa/training/view/View.java
+++ src/fa/training/view/View.java
@@ -8,7 +8,7 @@ public class View {
      * Show list of menu option except invalid option
      */
     public void showMenuOptionExceptInvalidOption() {
-        System.out.println("LIBARY MANAGEMENT");
+        System.out.println("LIBRARY MANAGEMENT");
         for(MenuOption menuOption: MenuOption.values()) {
             if (menuOption != MenuOption.INVALID_OPTION) {
                 System.out.println(menuOption.getLabel());
