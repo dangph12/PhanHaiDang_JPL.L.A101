96b6f17967b41cc3ac4d05bf8fb7bce432fe65c4
dangph12 <dangphhe181217@fpt.edu.vn>
2025-01-13 15:17:48 +0700

    chore: Add docs

--- src/fa/training/utils/Validator.java
+++ src/fa/training/utils/Validator.java
@@ -1,9 +1,21 @@
 package fa.training.utils;
 
 public class Validator {
+
+    /**
+     * Check if year after 1752
+     * @param year an integer value
+     * @return true if year after 1752
+     */
     public boolean isValidYear(int year) {
         return year > 1752;
     }
+
+    /**
+     * Check if valid isbn only contain 10-17 digit and '-' quotes
+     * @param isbn a string isbn
+     * @return true if isbn only contain 10-17 digit and '-' quotes
+     */
     public boolean isValidIsbn(String isbn) {
         String regex = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){0,7})?$)[\\d-]+$";
         return isbn.matches(regex);
